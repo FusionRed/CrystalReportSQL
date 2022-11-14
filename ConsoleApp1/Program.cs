925b1701306a099a6b8720491c74328df9f2e5ba
FusionRed <jwmcchurch@gmail.com>
2022-11-14 12:20:34 -0600

    Updating to fix command vs tables.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ using System;
 using System.IO;
 using System.Text;
 using System.Threading;
+using System.Web.UI.WebControls;
 
 namespace CrystalReportSQL
 {
@@ -29,12 +30,12 @@ namespace CrystalReportSQL
                     {
                         try
                         {
-                            Directory.CreateDirectory(String.Concat(pathway, "\\Report\\SQL\\"));
-                            Directory.CreateDirectory(String.Concat(pathway, "\\SubReport\\SQL\\"));
+                            Directory.CreateDirectory(String.Concat(pathway, "\\Report\\"));
+                            Directory.CreateDirectory(String.Concat(pathway, "\\SubReport\\"));
                         }
                         catch (Exception e)
                         {
-                            Console.WriteLine("Unable to create output pathway.  Create \\SQL\\ folder in filepath, then try again. Exception: { 0}", e.ToString());
+                            Console.WriteLine("Unable to create output folders.  Create folders for \\Report\\ and \\SubReport\\ in the file pathway, then try again. Exception: { 0}", e.ToString());
                             Console.WriteLine("\nPress any key to exit...");
                             Console.ReadKey();
                             System.Environment.Exit(0);
@@ -43,13 +44,13 @@ namespace CrystalReportSQL
                     foreach (string file in Directory.GetFiles(pathway, "*.rpt"))
                     {
                         var doc = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
-                        Console.WriteLine(String.Format("Processing For {0}...", file));
+                        Console.WriteLine(String.Format("Processing started for report: {0}...", file));
                         doc.Load(file);
                         int start = file.LastIndexOf("\\");
                         int end = file.LastIndexOf(".");
                         var length = (end - start) - 1;
                         string filename = file.Substring(start + 1, length);
-                        using (var sw = new StreamWriter(String.Concat(pathway, "\\Report\\SQL\\", filename, ".sql"), true, Encoding.Unicode))
+                        using (var sw = new StreamWriter(String.Concat(pathway, "\\Report\\", filename, ".txt"), true, Encoding.Unicode))
                         {
                             foreach (dynamic table in doc.ReportClientDocument.DatabaseController.Database.Tables)
                             {
@@ -69,31 +70,28 @@ namespace CrystalReportSQL
                             sw.Close();
                         }
                         Console.WriteLine(String.Format("Processing {0} for sub-reports...", file));
-                        if (doc.ReportClientDocument.SubreportController.GetSubreportNames() != null)
+                        foreach (string subName in doc.ReportClientDocument.SubreportController.GetSubreportNames())
                         {
-                            foreach (string subName in doc.ReportClientDocument.SubreportController.GetSubreportNames())
+                            CrystalDecisions.ReportAppServer.Controllers.SubreportClientDocument subRCD = doc.ReportClientDocument.SubreportController.GetSubreport(subName);
+                            using (var sw = new StreamWriter(String.Concat(pathway, "\\SubReport\\", filename, "-", subName, ".txt"), true, Encoding.Unicode))
                             {
-                                CrystalDecisions.ReportAppServer.Controllers.SubreportClientDocument subRCD = doc.ReportClientDocument.SubreportController.GetSubreport(subName);
-                                using (var sw = new StreamWriter(String.Concat(pathway, "\\SubReport\\SQL\\",filename,"-", subName, ".sql"), true, Encoding.Unicode))
+                                var boDatabase = subRCD.DataDefController.Database;
+                                foreach (dynamic subtable in boDatabase.Tables)
                                 {
-                                    var boDatabase = subRCD.DataDefController.Database;
-                                    foreach (dynamic subtable in boDatabase.Tables)
+                                    if (subtable.ClassName == "CrystalReports.CommandTable")
                                     {
-                                        if (subtable.ClassName == "CrystalReports.CommandTable")
-                                        {
-                                            string commandSql = subtable.CommandText;
-                                            Console.WriteLine(String.Format("Writing SQL for {0} sub-report...", subName));
-                                            sw.WriteLine(commandSql);
-                                        }
-                                        else
-                                        {
-                                            string tableSql = subtable.Name;
-                                            Console.WriteLine(String.Format("Writing tables for {0} sub-report...", subName));
-                                            sw.WriteLine(tableSql);
-                                        }
+                                        string commandSql = subtable.CommandText;
+                                        Console.WriteLine(String.Format("Writing SQL for {0} sub-report...", subName));
+                                        sw.WriteLine(commandSql);
                                     }
-                                    sw.Close();
+                                    else
+                                    {
+                                        string tableSql = subtable.Name;
+                                        Console.WriteLine(String.Format("Writing tables for {0} sub-report...", subName));
+                                        sw.WriteLine(tableSql);
+                                    }
                                 }
+                                sw.Close();
                             }
                         }
                         doc.Close();
